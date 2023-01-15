4b27e35dc25deb415010d2ea559cebd93ab796ff
sergqwer <38361819+sergqwer@users.noreply.github.com>
2023-01-15 12:15:49 +0200

    Update app.py

--- p2proxy/app.py
+++ p2proxy/app.py
@@ -9,5 +9,5 @@ app = Starlette(routes=routes)
 
 if __name__ == "__main__":
     config = Config()
-    config.bind = "0.0.0.0:3600"
+    config.bind = "0.0.0.0:80"
     asyncio.run(serve(app, config))
